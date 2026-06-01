a7a9d417259d224477aafc95a1080c585e6471e4
Farotech Lab <42328129+FAROTECH@users.noreply.github.com>
2026-06-01 17:08:17 +0200

    update model atlas command surface

--- src/CommandabilityDomainSurface.tsx
+++ src/CommandabilityDomainSurface.tsx
@@ -1,3 +1,6 @@
 import { MissionModelAtlasSurface } from "./MissionModelAtlasSurface";
+import type { MissionModelAtlasSurfaceProps } from "./MissionModelAtlasSurface";
 
-export const CommandabilityDomainSurface = MissionModelAtlasSurface;
+export function CommandabilityDomainSurface(props: MissionModelAtlasSurfaceProps) {
+  return <MissionModelAtlasSurface {...props} preferredDomainId="commandability" />;
+}
