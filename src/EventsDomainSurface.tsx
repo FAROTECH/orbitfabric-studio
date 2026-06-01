8097362042c6d5c993aaf26befe13a1ab592b037
Farotech Lab <42328129+FAROTECH@users.noreply.github.com>
2026-06-01 17:00:50 +0200

    refactor: bind events atlas entrypoint

--- src/EventsDomainSurface.tsx
+++ src/EventsDomainSurface.tsx
@@ -1,1 +1,8 @@
-export { MissionModelAtlasSurface as EventsDomainSurface } from "./MissionModelAtlasSurface";
+import {
+  MissionModelAtlasSurface,
+  type MissionModelAtlasSurfaceProps,
+} from "./MissionModelAtlasSurface";
+
+export function EventsDomainSurface(props: MissionModelAtlasSurfaceProps) {
+  return <MissionModelAtlasSurface {...props} preferredDomainId="events" />;
+}
