0c84f61abd41a0caa2278d18e66ae7630f50fc3f
Farotech Lab <42328129+FAROTECH@users.noreply.github.com>
2026-06-01 16:59:21 +0200

    refactor: bind telemetry atlas entrypoint

--- src/TelemetryDomainSurface.tsx
+++ src/TelemetryDomainSurface.tsx
@@ -1,1 +1,8 @@
-export { MissionModelAtlasSurface as TelemetryDomainSurface } from "./MissionModelAtlasSurface";
+import {
+  MissionModelAtlasSurface,
+  type MissionModelAtlasSurfaceProps,
+} from "./MissionModelAtlasSurface";
+
+export function TelemetryDomainSurface(props: MissionModelAtlasSurfaceProps) {
+  return <MissionModelAtlasSurface {...props} preferredDomainId="telemetry" />;
+}
