f543710da71d1e670a97a3fab07bd806fb92694f
Farotech Lab <42328129+FAROTECH@users.noreply.github.com>
2026-06-01 13:48:00 +0200

    style: load scenario runway console experiment

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./missionDataFlowWorkbenchVisualHierarchy.css";
 import "./generatedArtifactExplorer.css";
 import "./releaseHardening.css";
 import "./scenarioTimelineRunner.css";
+import "./scenarioRunwayConsole.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
