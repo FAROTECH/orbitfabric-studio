98110fb430d8a444a261991a68f2ada1c6ad71d5
Fabrizio Rovelli <rovelli@gmail.com>
2026-06-01 10:45:44 +0200

    style: align mission data flow workbench with evidence route canvas

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./missionCockpitVisualHierarchy.css";
+import "./missionDataFlowWorkbenchVisualHierarchy.css";
 import "./generatedArtifactExplorer.css";
 import "./releaseHardening.css";
 
