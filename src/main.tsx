97ede46e617446ee37b4f97af79dc9d9c7e6f73d
Fabrizio Rovelli <rovelli@gmail.com>
2026-06-06 11:12:54 +0200

    style: refine data flow workbench visual focus

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./missionCockpitVisualHierarchy.css";
 import "./missionContentCockpit.css";
 import "./missionWorkspaceTopbarHeroFix.css";
 import "./missionDataFlowWorkbenchVisualHierarchy.css";
+import "./missionDataFlowWorkbenchStep2.css";
 import "./generatedArtifactExplorer.css";
 import "./generatedArtifactDeck.css";
 import "./generatedArtifactDeckStabilization.css";
