cf4fc4757844756839384788ffae8ab38ce1c0b2
Farotech Lab <42328129+FAROTECH@users.noreply.github.com>
2026-08-17 10:03:24 +0200

    style: activate Explorer and X-Ray styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./styles/tokens.css";
 import "./styles/reset.css";
 import "./styles/shell.css";
+import "./styles/features.css";
 import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
