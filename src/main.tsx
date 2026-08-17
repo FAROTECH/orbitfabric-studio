6b2340c652f467d3b3130707b18997d6a42fee4a
Farotech Lab <42328129+FAROTECH@users.noreply.github.com>
2026-08-17 10:12:31 +0200

    style: activate Relations and Context Map styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./styles/tokens.css";
 import "./styles/reset.css";
 import "./styles/shell.css";
 import "./styles/features.css";
+import "./styles/relations.css";
 import "./styles/responsive.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
